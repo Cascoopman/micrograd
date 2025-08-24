e2a81980497f9aef6dd08a3159f77c92ead5e6b5
Cascoopman <cascoopman@hotmail.com>
2025-08-24 20:38:29 +0200

    Add Value class for automatic gradients

---
+++ README.md
@@ -0,0 +1,4 @@
+# Repository info
+
+This repo contains the code to learn about backpropagation and auto differentiation as used in PyTorch.
+This code is based on Andrej Karpathy's first [video](https://www.youtube.com/watch?v=VMj-3S1tku0) in the Neural networks - Zero to Hero series.
---
+++ engine.py
@@ -0,0 +1,102 @@
+import math
+
+
+class Value:
+    def __init__(self, data, _children=(), _op=""):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
+
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other: "Value"):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), (
+            "Only supporting ints and floats for now."
+        )
+        out = Value(self.data**other, (self,), f"**{other}")
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1.0)) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * other**-1
+
+    def __neg__(self):
+        return self * -1.0
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.e ** (x), (self,), "exp")
+
+        def _backward():
+            self.grad += out.grad * out.data
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def _build_topo(node: Value):
+            if node not in visited:
+                visited.add(node)
+                for child in node._prev:
+                    _build_topo(child)
+                topo.append(node)
+
+        _build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
